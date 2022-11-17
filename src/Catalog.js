e1b0f4d9fe719d82fa6fc9e843b3e356f005b10d
Miya Furikadze <50241874+cutemiya@users.noreply.github.com>
2022-11-17 23:03:19 +0300

    Update Catalog.js

--- src/Catalog.js
+++ src/Catalog.js
@@ -1,10 +1,14 @@
 import Card from "./Card";
 import "./css/Catalog.css"
 import {favourites_images} from "./Card";
+import {useState} from "react";
 
 const url = {
     prefixUrl: "https://www.reddit.com/r/cats/top/.json"
 }
+
+let start = 0
+let count = 5;
 
 function httpGet(theUrl) {
     let xmlHttp = new XMLHttpRequest();
@@ -18,8 +22,8 @@ function urlImages(response) {
     let url;
     let urlArray = [];
 
-    for (let i = 0; i < 25; i++) {
-        url = json["data"]["children"][i]["data"]["url"]
+    for (start; start < count; start++) {
+        url = json["data"]["children"][start]["data"]["url"]
         let format = url.slice(-4)
         if (format === ".jpg" || format === ".png") {
             if (favourites_images.indexOf(url) === -1) {
@@ -27,16 +31,38 @@ function urlImages(response) {
             }
         }
     }
+    count += 5;
+
     return urlArray
 }
 
 export default function Catalog() {
+    const [cats, setCats] = useState([])
 
     return (
         <div className="catalog__container">
-            {urlImages(httpGet(url.prefixUrl)).map((url) => (
-                <Card src={url} type={0}/>
-            ))}
+            <h1>Cats</h1>
+            {cats}
+            <AddCats cats={cats} setCats={setCats}/>
         </div>
+    )
+}
+
+function AddCats({ setCats }) {
+    function handlerAddCats() {
+        urlImages(httpGet(url.prefixUrl)).map((url) =>
+            setCats((cats) => cats.concat(<Card src={url} type={0} />))
+        )
+    }
+
+    const buttonStyle = {
+        marginLeft: "350px",
+        maxWidth: "75px",
+        width: "100%",
+        background: "#f6bcbc"
+    }
+
+    return (
+        <button onClick={handlerAddCats} style={buttonStyle}>More cats</button>
     )
 }
